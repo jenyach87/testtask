d79e38db8a88a4a0fe08605132de03c04abaf4e7
jenyach87 <jenya_87ch@ukr.net>
2024-06-26 14:31:06 +0300

    changes in header

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -5,7 +5,7 @@ import { Button } from '@nextui-org/react';
 
 const HeaderSection: React.FC = () => {
   return (
-    <header className="w-full  h-28 lg:h-16 p-4 lg:p-0 flex justify-between items-center mx-auto  max-w-1088 border-b-1  border-gray-200  lg:mb-5">
+    <header className="w-full  h-28 lg:h-16 p-4 lg:p-0 flex justify-between items-end lg:items-center mx-auto  max-w-1088 border-b-1  border-gray-200  lg:mb-5">
       <nav className="w-full flex justify-between items-center">
         <Link href="/">
           <Image src="/leefstylist_logo 1.jpg" className='h-10 w-32' alt="Leefstylist Logo" width={100} height={104} />
